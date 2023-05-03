a314703eea5e3a821d5591cc9bf49bc12d036b98
a-programmer <mrsadin@gmail.com>
2023-05-03 18:31:26 +0330

    Add Pagination helper

---
+++ src/Sadin.Common/Pagination/Paginated.cs
@@ -0,0 +1,14 @@
+namespace Sadin.Common.Pagination;
+
+public class Paginated
+{
+    public Paginated(int? pageNumber, int? pageSize)
+    {
+        PageIndex = pageNumber ?? 1;
+        PageSize = pageSize ?? 10;
+    }
+
+
+    public int PageIndex { get; private set; }
+    public int PageSize { get; private set; }
+}
